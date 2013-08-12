dba3733ac4ee2e8c0c6f45b00fed3cfbffa7465b
Chris Friesen <friesencr@gmail.com>
2013-08-12 18:22:15 -0500

    update builds

--- Gemfile
+++ Gemfile
@@ -1,7 +1,10 @@
-source :rubygems
+source 'https://rubygems.org'
 
 gem "middleman", "~> 3.1.4"
 gem "middleman-blog", "~> 3.2.0"
+gem "middleman-deploy"
+gem "middleman-minify-html"
+
 
 # For feed.xml.builder
 gem "builder", "~> 3.0.0"
--- config.rb
+++ config.rb
@@ -28,6 +28,11 @@ activate :blog do |blog|
   # blog.page_link = "page/:num"
 end
 
+activate :deploy do |deploy|
+    deploy.build_before = true
+    deploy.method = :git
+end
+
 page "/feed.xml", :layout => false
 
 ### 
@@ -88,10 +93,13 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
   
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+
+  # Minify html
+  activate :minify_html
   
   # Enable cache buster
   # activate :cache_buster
